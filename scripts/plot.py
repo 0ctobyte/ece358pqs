0cfcfad897706a1b6964d4f0248004c75916968e
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2015-02-07 12:35:00 -0500

    Fixed labelling in plot.py

--- scripts/plot.py
+++ scripts/plot.py
@@ -18,20 +18,20 @@ pylab.figure(++figure)
 pylab.clf()
 data, label = pylab.loadtxt(filelist[0],delimiter=','), r'K=$\infty$'
 pylab.plot(data[:,0], data[:,1],label=label)
-pylab.legend()
 pylab.title(r"$E[N]$ vs $\rho$")
 pylab.xlabel(r"Utilization of the buffer")
 pylab.ylabel(r"Average \# of packets in the buffer")
+pylab.legend()
 pylab.savefig(filelist[0].rsplit('.',1)[0]+".png")
 
 pylab.figure(++figure)
 pylab.clf()
 data, label = pylab.loadtxt(filelist[0],delimiter=','), r'K=$\infty$'
 pylab.plot(data[:,0], data[:,1], label=label)
-pylab.legend()
 pylab.title(r"$P_{idle}$ vs $\rho$")
 pylab.xlabel(r"Utilization of the buffer")
 pylab.ylabel(r"Proportion of time the system is idle")
+pylab.legend()
 pylab.savefig(filelist[1].rsplit('.',1)[0]+".png")
 
 call(["./sim", "-t 10000", "-c 1000000", "-l 12000", "-r 0.5,1.5", "-k 5"])
@@ -42,14 +42,14 @@ pylab.figure(++figure)
 pylab.clf()
 data, label = pylab.loadtxt(filelist[3],delimiter=','), 'K=5'
 pylab.plot(data[:,0], data[:,1], label=label)
-pylab.legend()
-pylab.title(r"$E[N]$ vs $\rho$")
-pylab.xlabel(r"Utilization of the buffer")
-pylab.ylabel(r"Average \# of packets in the buffer")
 data, label = pylab.loadtxt(filelist[6],delimiter=','), 'K=10'
 pylab.plot(data[:,0], data[:,1], label=label)
 data, label = pylab.loadtxt(filelist[9],delimiter=','), 'K=40'
 pylab.plot(data[:,0], data[:,1], label=label)
+pylab.title(r"$E[N]$ vs $\rho$")
+pylab.xlabel(r"Utilization of the buffer")
+pylab.ylabel(r"Average \# of packets in the buffer")
+pylab.legend()
 pylab.savefig("en_vs_rho_k_finite.png")
 
 call(["./sim", "-t 10000", "-c 1000000", "-l 12000", "-r 0.4,10", "-k 5"])
@@ -60,13 +60,13 @@ pylab.figure(++figure)
 pylab.clf()
 data, label = pylab.loadtxt(filelist[5],delimiter=','), 'K=5'
 pylab.plot(data[:,0], data[:,1], label=label)
-pylab.legend()
-pylab.title(r"$P_{loss}$ vs $\rho$")
-pylab.xlabel(r"Utilization of the buffer")
-pylab.ylabel(r"Packet loss ratio")
 data, label = pylab.loadtxt(filelist[8],delimiter=','), 'K=10'
 pylab.plot(data[:,0], data[:,1], label=label)
 data, label = pylab.loadtxt(filelist[11],delimiter=','), 'K=40'
 pylab.plot(data[:,0], data[:,1], label=label)
+pylab.title(r"$P_{loss}$ vs $\rho$")
+pylab.xlabel(r"Utilization of the buffer")
+pylab.ylabel(r"Packet loss ratio")
+pylab.legend()
 pylab.savefig("ploss_vs_rho_k_finite.png")
 
