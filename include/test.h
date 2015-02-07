7e8dbe9caa03a62cabe92f7516d995590ff76e32
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2015-02-06 22:31:21 -0500

    Changed function name in test.h

--- include/test.h
+++ include/test.h
@@ -1,8 +1,8 @@
 #ifndef __TEST_H__
 #define __TEST_H__
 
-void test_randvar_uniform();
-void test_randvar_exp(double lambda);
+void test_rv_uniform();
+void test_rv_exp(double lambda);
 void test_es_pq();
 
 #endif // __TEST_H__
